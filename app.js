502933bb0da145253c1340c791831331c71b5efe
meghna114 <meghna.kaushik@arthmate.co.in>
2024-07-23 16:22:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables
+require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
